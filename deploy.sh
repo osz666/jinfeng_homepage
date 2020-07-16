52a67a068af73df0bd9b51d617e02a5e1a8c6a6b
Lucien Wang <lucienwang@qq.com>
2020-07-16 17:27:49 +0800

    add ssh key when deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@
 
 set -e
 
+openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d;
+eval "$(ssh-agent -s)";
+cp .travis/deploy_rsa ~/.ssh/deploy_rsa;
+chmod 600 ~/.ssh/deploy_rsa;
+ssh-add ~/.ssh/deploy_rsa;
+
 rsync -azP ./front/dist/ root@47.102.196.249:/var/www/dist/
